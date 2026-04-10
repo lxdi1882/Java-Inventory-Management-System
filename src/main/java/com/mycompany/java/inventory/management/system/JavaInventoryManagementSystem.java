36146ea2c2e4de30ae580a84bf2ea7e38c3d0cb0
aleiyah <aleiyahelisan2.0@gmail.com>
2026-04-10 13:41:09 +0800

    commit sample

--- src/main/java/com/mycompany/java/inventory/management/system/JavaInventoryManagementSystem.java
+++ src/main/java/com/mycompany/java/inventory/management/system/JavaInventoryManagementSystem.java
@@ -12,5 +12,7 @@ public class JavaInventoryManagementSystem {
 
     public static void main(String[] args) {
         System.out.println("helo fren");
+        
+        System.out.println("wasp bbg");
     }
 }
