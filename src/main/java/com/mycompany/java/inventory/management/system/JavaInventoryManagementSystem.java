f9257c574ea813544631063f9a068b4646f3a04a
lxdi1882 <lxdi1883@gmail.com>
2026-04-10 12:51:28 +0800

    Initial commit

---
+++ src/main/java/com/mycompany/java/inventory/management/system/JavaInventoryManagementSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java.inventory.management.system;
+
+/**
+ *
+ * @author lxdi1
+ */
+public class JavaInventoryManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
