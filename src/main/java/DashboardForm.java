f887a9816a73f6370146b1ff055d22da2c4b5b55
lxdi1882 <lxdi1883@gmail.com>
2026-04-28 22:08:28 +0800

    cleaned and organized the folders
    refactored the .java files

--- src/main/java/DashboardForm.java
+++ src/main/java/DashboardForm.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.java.inventory.management.system;
+
 
 /**
  *
  * @author lxdi1
  */
-public class ProductForm {
+public class DashboardForm {
     
 }
--- src/main/java/Project2InventorySystemBSCS1A.java
+++ src/main/java/Project2InventorySystemBSCS1A.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package Project2InventorySystemBSCS1A;
+
 
 /**
  *
@@ -13,7 +13,7 @@ public class Project2InventorySystemBSCS1A {
 
     public static void main(String[] args) {
         System.out.println("helo fren");
-        
+       
         System.out.println("wasp bbg");
     }
 }
